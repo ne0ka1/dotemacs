aad564f13187973e55de632f04e8dd8cea1381b1
Kaixuan Zhang <kai.xuan.zhang@outlook.com>
2024-11-03 18:49:54 +0800

    remove init-shell.el

--- lisp/init-shell.el
+++
@@ -1,8 +0,0 @@
-;;; init-shell.el --- config for shells
-(straight-use-package 'shell-pop)       ; https://github.com/kyagi/shell-pop-el
-
-;; shell-pop
-(require 'shell-pop)
-(setq shell-pop-universal-key "C-`")
-
-(provide 'init-shell)
